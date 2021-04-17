4aa09a8873eedd26b8a681182f86badf6e8ec05d
fixmol <fixmol@mail.ru>
2021-04-17 23:44:20 +0300

    Added the main component and styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
+  <div class="container">
+
+    <header class="header">
+      <button class="btn active">News Block</button>
+      <button class="btn">Slots Block</button>
+      <button class="btn">Compon Block</button>
+    </header>
+
+    <news-app>
+    </news-app>
+
+  </div>
 </template>
 
 
 <script>
-export default {
+import NewsApp from './components/news/NewsApp'
 
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: {
+    'news-app': NewsApp
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './theme.css'
 
 const app = createApp(App)
 app.mount('#app')
